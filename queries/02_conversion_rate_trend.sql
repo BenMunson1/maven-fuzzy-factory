aadb02b1fd9b968ce715fd3ad80fb178294396da
Ben Munson <100311728+BenMunson1@users.noreply.github.com>
2025-11-05 19:47:04 -0600

    Update 02_conversion_rate_trend.sql

--- queries/02_conversion_rate_trend.sql
+++ queries/02_conversion_rate_trend.sql
@@ -8,22 +8,31 @@ and analyze how the conversion rate has trended.
 */
 
 WITH scte AS (
-    SELECT strftime('%Y-%m', created_at) AS year_month,
-           COUNT(website_session_id) AS session_count
-      FROM website_sessions
-     GROUP BY year_month
+    SELECT 
+         strftime('%Y-%m', created_at) AS year_month,
+         COUNT(website_session_id) AS session_count
+     FROM 
+         website_sessions
+     GROUP BY 
+         year_month
 ),
 octe AS (
-    SELECT strftime('%Y-%m', created_at) AS year_month,
-           COUNT(order_id) AS order_count
-      FROM orders
-     GROUP BY year_month
+    SELECT 
+         strftime('%Y-%m', created_at) AS year_month,
+         COUNT(order_id) AS order_count
+     FROM 
+         orders
+     GROUP BY 
+         year_month
 )
-SELECT scte.year_month,
-       scte.session_count,
-       COALESCE(octe.order_count, 0) AS order_count,
-       ROUND((COALESCE(octe.order_count, 0) * 1.0 / scte.session_count) * 100, 2) AS conversion_rate
-  FROM scte
-       LEFT JOIN
-       octe ON scte.year_month = octe.year_month
- ORDER BY scte.year_month;
+SELECT 
+    scte.year_month,
+    scte.session_count,
+    COALESCE(octe.order_count, 0) AS order_count,
+    ROUND((COALESCE(octe.order_count, 0) * 1.0 / scte.session_count) * 100, 2) AS conversion_rate
+FROM 
+    scte
+LEFT JOIN
+    octe ON scte.year_month = octe.year_month
+ORDER BY 
+    scte.year_month;
