406280a3c114b7ea8a8230faea321578f5ddce99
Ben Munson <100311728+BenMunson1@users.noreply.github.com>
2025-11-05 19:45:18 -0600

    Update 01_monthly_sessions_vs_orders.sql

--- queries/01_monthly_sessions_vs_orders.sql
+++ queries/01_monthly_sessions_vs_orders.sql
@@ -8,21 +8,30 @@ to understand overall growth patterns and user engagement over time.
 */
 
 WITH scte AS (
-    SELECT strftime('%Y-%m', created_at) AS year_month,
-           COUNT(website_session_id) AS session_count
-      FROM website_sessions
-     GROUP BY year_month
+    SELECT 
+        strftime('%Y-%m', created_at) AS year_month,
+        COUNT(website_session_id) AS session_count
+     FROM 
+        website_sessions
+     GROUP BY 
+        year_month
 ),
 octe AS (
-    SELECT strftime('%Y-%m', created_at) AS year_month,
-           COUNT(order_id) AS order_count
-      FROM orders
-     GROUP BY year_month
+    SELECT 
+        strftime('%Y-%m', created_at) AS year_month,
+        COUNT(order_id) AS order_count
+     FROM 
+        orders
+     GROUP BY 
+        year_month
 )
-SELECT scte.year_month,
-       scte.session_count,
-       COALESCE(octe.order_count, 0) AS order_count
-  FROM scte
-       LEFT JOIN
-       octe ON scte.year_month = octe.year_month
- ORDER BY scte.year_month;
+SELECT 
+    scte.year_month,
+    scte.session_count,
+    COALESCE(octe.order_count, 0) AS order_count
+FROM 
+    scte
+LEFT JOIN
+    octe ON scte.year_month = octe.year_month
+ORDER BY 
+    scte.year_month;
