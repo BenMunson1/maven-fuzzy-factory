887abed4b0a4b10f45ed0661fd14dd0bd8bb8728
Ben Munson <100311728+BenMunson1@users.noreply.github.com>
2025-11-09 09:34:01 -0600

    Update 02_conversion_rate_trend.sql

--- queries/02_conversion_rate_trend.sql
+++ queries/02_conversion_rate_trend.sql
@@ -7,32 +7,19 @@ Determine how effectively website sessions convert into customer orders over tim
 and analyze how the conversion rate has trended.
 */
 
-WITH scte AS (
-    SELECT 
-         strftime('%Y-%m', created_at) AS year_month,
-         COUNT(website_session_id) AS session_count
-     FROM 
-         website_sessions
-     GROUP BY 
-         year_month
-),
-octe AS (
-    SELECT 
-         strftime('%Y-%m', created_at) AS year_month,
-         COUNT(order_id) AS order_count
-     FROM 
-         orders
-     GROUP BY 
-         year_month
+WITH monthly_activity AS (
+    SELECT strftime('%Y-%m', ws.created_at) AS year_month,
+           COUNT(ws.website_session_id) AS session_count,
+           COUNT(o.order_id) AS order_count
+      FROM website_sessions ws
+           LEFT JOIN
+           orders o ON ws.website_session_id = o.website_session_id
+     GROUP BY year_month
 )
-SELECT 
-    scte.year_month,
-    scte.session_count,
-    COALESCE(octe.order_count, 0) AS order_count,
-    ROUND((COALESCE(octe.order_count, 0) * 1.0 / scte.session_count) * 100, 2) AS conversion_rate
-FROM 
-    scte
-LEFT JOIN
-    octe ON scte.year_month = octe.year_month
-ORDER BY 
-    scte.year_month;
+SELECT year_month,
+       session_count,
+       COALESCE(order_count, 0) AS order_count,
+       ROUND((COALESCE(order_count, 0) * 1.0 / session_count) * 100, 2) AS conversion_rate,
+       ROUND(AVG((COALESCE(order_count, 0) * 1.0 / session_count) * 100) OVER (ORDER BY year_month ROWS BETWEEN 2 PRECEDING AND CURRENT ROW), 2) AS rolling_3mo_avg
+  FROM monthly_activity
+ ORDER BY year_month;
