2fb27037089f8311281fb83645e84bf8d519ffb7
Ben Munson <100311728+BenMunson1@users.noreply.github.com>
2025-11-09 09:36:27 -0600

    Update 01_monthly_sessions_vs_orders.sql

--- queries/01_monthly_sessions_vs_orders.sql
+++ queries/01_monthly_sessions_vs_orders.sql
@@ -7,31 +7,10 @@ Analyze the month-over-month trend in total website sessions and total orders
 to understand overall growth patterns and user engagement over time.
 */
 
-WITH scte AS (
-    SELECT 
-        strftime('%Y-%m', created_at) AS year_month,
-        COUNT(website_session_id) AS session_count
-     FROM 
-        website_sessions
-     GROUP BY 
-        year_month
-),
-octe AS (
-    SELECT 
-        strftime('%Y-%m', created_at) AS year_month,
-        COUNT(order_id) AS order_count
-     FROM 
-        orders
-     GROUP BY 
-        year_month
-)
-SELECT 
-    scte.year_month,
-    scte.session_count,
-    COALESCE(octe.order_count, 0) AS order_count
-FROM 
-    scte
-LEFT JOIN
-    octe ON scte.year_month = octe.year_month
-ORDER BY 
-    scte.year_month;
+SELECT strftime('%Y-%m', ws.created_at) AS year_month,
+       COUNT(ws.website_session_id) AS session_count,
+       COUNT(o.order_id) AS order_count
+  FROM website_sessions ws
+       LEFT JOIN
+       orders o ON ws.website_session_id = o.website_session_id
+ GROUP BY year_month;
