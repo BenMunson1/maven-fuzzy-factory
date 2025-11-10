ca013158beaf1dd862f0c918d3a1e6a62b46e50b
Ben Munson <100311728+BenMunson1@users.noreply.github.com>
2025-11-09 18:54:12 -0600

    Update 03_channel_performance.sql

--- queries/03_channel_performance.sql
+++ queries/03_channel_performance.sql
@@ -11,8 +11,8 @@ WITH channel_performance AS (
           CASE WHEN ws.utm_source = 'NULL' THEN 'Direct / Organic' ELSE ws.utm_source END AS channel,
           COUNT(DISTINCT ws.website_session_id) AS session_count,
           COUNT(DISTINCT o.order_id) AS order_count,
-          AVG(o.price_usd) AS avg_order_value,
-          SUM(o.price_usd) AS total_revenue
+          ROUND(AVG(o.price_usd), 2) AS avg_order_value,
+          ROUND(SUM(o.price_usd), 2) AS total_revenue
       FROM 
           website_sessions ws
       LEFT JOIN
@@ -25,7 +25,7 @@ SELECT
     session_count, 
     order_count, 
     ROUND((order_count * 1.0 / session_count) * 100, 2) AS conversion_rate,
-    ROUND(avg_order_value, 2) AS avg_order_value,
+    avg_order_value,
     ROUND((total_revenue * 1.0 / session_count), 2) AS revenue_per_session
 FROM 
     channel_performance
