987b35d77c910296beaea8eb4b435fa072988f2f
Ben Munson <100311728+BenMunson1@users.noreply.github.com>
2025-11-09 18:50:26 -0600

    Update 04_revenue_metrics.sql

--- queries/04_revenue_metrics.sql
+++ queries/04_revenue_metrics.sql
@@ -8,11 +8,11 @@ Determine how both average order value (AOV) and revenue per session have evolve
 
 WITH orders_by_month AS (
     SELECT strftime('%Y-%m', created_at) AS year_month,
-           SUM(price_usd) AS total_revenue,
-           SUM(price_usd - cogs_usd) AS total_gross_margin,
+           ROUND(SUM(price_usd), 2) AS total_revenue,
+           ROUND(SUM(price_usd - cogs_usd), 2) AS total_gross_margin,
            COUNT(order_id) AS num_orders,
-           AVG(price_usd) AS avg_order_value,
-           AVG(price_usd - cogs_usd) AS avg_gross_margin_per_order
+           ROUND(AVG(price_usd), 2) AS avg_order_value,
+           ROUND(AVG(price_usd - cogs_usd), 2) AS avg_gross_margin_per_order
       FROM orders
      GROUP BY year_month
 ),
@@ -25,11 +25,11 @@ sessions_by_month AS (
 SELECT sbm.year_month,
        obm.num_orders,
        sbm.num_sessions,
-       ROUND(obm.total_revenue, 2) AS total_revenue,
-       ROUND(obm.avg_order_value, 2) AS avg_order_value,
+       obm.total_revenue,
+       obm.avg_order_value,
        ROUND((obm.total_revenue * 1.0 / sbm.num_sessions), 2) AS revenue_per_session,
-       ROUND(obm.total_gross_margin, 2) AS total_gross_margin,
-       ROUND(obm.avg_gross_margin_per_order, 2) AS avg_gross_margin_per_order,
+       obm.total_gross_margin,
+       obm.avg_gross_margin_per_order,
        ROUND((obm.total_gross_margin * 1.0 / sbm.num_sessions), 2) AS gross_margin_per_session
   FROM sessions_by_month sbm
        LEFT JOIN
